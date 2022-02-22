bb57e20441fab4aadb395042a8878e69a3015783
Nanda Refiani <nandarefiani07@gmail.com>
2022-02-22 19:59:14 +0800

    Membuat Perintah Hapus Data

---
+++ pages/admin/lokasidelete.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $deleteSQL = "DELETE FROM lokasi WHERE id= ?";
+    $stmt = $db->prepare($deleteSQL);
+    $stmt->bindParam(1, $_GET['id']);
+    if ($stmt->execute()){
+        $_SESSION['hasil']= true;
+        $_SESSION['pesan']= "Hapus Berhasil";
+    } else {
+        $_SESSION['hasil']= false;
+        $_SESSION['pesan']= "Hapus Gagal";
+    }
+}
+echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
